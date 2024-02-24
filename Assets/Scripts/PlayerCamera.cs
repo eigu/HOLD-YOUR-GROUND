50ac961d6b6901c253d75073a0195e0e5c50c5b3
Jerico Galang <jmpheen@gmail.com>
2024-02-24 21:05:38 +0800

    Changes

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -4,7 +4,7 @@ public class PlayerCamera : MonoBehaviour
 {
     private PlayerPowerUps playerPowerUp;
 
-    private Vector3 playerOrientation;
+    [SerializeField] private Vector3 playerOrientation;
     public Transform cameraHolder;
     [SerializeField] private Transform playerHand;
 
@@ -25,44 +25,42 @@ public class PlayerCamera : MonoBehaviour
 
     private void Look()
     {
+        Vector3 mouseDelta = InputManager.Instance.GetMouseDelta();
+
+        mouseDelta *= sensitivity;
+
+        smoothedDelta = Vector2.Lerp(smoothedDelta, mouseDelta, 1f / smoothing);
+
         if (playerPowerUp.lockedEnemy != null)
         {
-            //cameraHolder.LookAt(playerPowerUp.lockedEnemy.transform);
-            //transform.LookAt(playerPowerUp.lockedEnemy.transform);
-
             var lockedEnemyPosition = playerPowerUp.lockedEnemy.transform.position;
 
             Vector3 lookDirection = lockedEnemyPosition - cameraHolder.position;
 
             Quaternion horizontalRotation = Quaternion.LookRotation(new Vector3(lookDirection.x, 0, lookDirection.z));
 
+
+            playerOrientation.y = -lockedEnemyPosition.y;
+
             cameraHolder.rotation = Quaternion.Euler(-lockedEnemyPosition.y, horizontalRotation.eulerAngles.y, 0);
             transform.rotation = Quaternion.Euler(0, horizontalRotation.eulerAngles.y, 0);
         }
         else
         {
-            Vector3 mouseDelta = InputManager.Instance.GetMouseDelta();
-
-            mouseDelta *= sensitivity;
-
-            smoothedDelta = Vector2.Lerp(smoothedDelta, mouseDelta, 1f / smoothing);
-
             playerOrientation.x += mouseDelta.x;
             playerOrientation.y -= mouseDelta.y;
-
-            playerOrientation.x = Mathf.Repeat(playerOrientation.x + 180f, 360f) - 180f;
-            playerOrientation.y = Mathf.Clamp(playerOrientation.y, -30f, 30f);
-
-            cameraHolder.rotation = Quaternion.Euler(new Vector3(playerOrientation.y, cameraHolder.rotation.eulerAngles.y, 0));
-
-            transform.rotation = Quaternion.Euler(new Vector3(0, playerOrientation.x, 0));
-
-            
         }
+
+        playerOrientation.x = Mathf.Repeat(playerOrientation.x + 180f, 360f) - 180f;
+        playerOrientation.y = Mathf.Clamp(playerOrientation.y, -30f, 30f);
+
+        cameraHolder.rotation = Quaternion.Euler(new Vector3(playerOrientation.y, cameraHolder.rotation.eulerAngles.y, 0));
+        transform.rotation = Quaternion.Euler(new Vector3(0, playerOrientation.x, 0));
 
         if (playerHand != null)
         {
             playerHand.LookAt(InputManager.Instance.GetCrosshairPoint());
         }
     }
+
 }
